793d1275164f68e1af60bd72242d808565cae930
colinchriston <colin.cd@surya-digital.com>
2024-03-04 16:18:39 +0530

    Create an calculator file

---
+++ src/Calculator.kt
@@ -0,0 +1,9 @@
+fun main(args:Array<String>){
+    print("Input expression: ")
+    var input= readLine()
+    print("ur input is ${input}")
+    while (true) {
+        println("Wanna restart again (Y/N)?")
+        var restart = readln()
+    }
+}
